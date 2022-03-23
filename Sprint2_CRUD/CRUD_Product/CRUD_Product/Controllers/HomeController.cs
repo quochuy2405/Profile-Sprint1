5789e2297d4e0d4d90bdfc8131a3a4b4260c9a8b
Bùi Quốc Huy <huy01682312920@gmail.com>
2022-03-23 10:26:42 +0700

    Crud

---
+++ Sprint2_CRUD/CRUD_Product/CRUD_Product/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using CRUD_Product.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CRUD_Product.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public List<Product> Products = new List<Product>();
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {       
+  
+            return View(Products);
+        }
+        [HttpPost]
+        public void ADD_Product(Product product)
+        {
+            Console.WriteLine("add");
+            Products.Add(product);
+       
+        }
+    }
+}
---
+++ Sprint2_CRUD/CRUD_Product/CRUD_Product/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace CRUD_Product.Models
+{
+    public class Product 
+    {
+        private string name;
+        private string category;
+        private float price;
+
+        public string Name { get => name; set => name = value; }
+        public string Category { get => category; set => category = value; }
+        public float Price { get => price; set => price = value; }
+    }
+}
